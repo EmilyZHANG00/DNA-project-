70c03d99fe8133316cb2c8b2ccc18a6390780d1f
emily <1151917861@qq.com>
2024-12-04 19:37:21 +0800

    archive
    Signed-off-by: emily <1151917861@qq.com>

--- source/ConstraintTest.py
+++ source/ConstraintTest.py
@@ -1,18 +1,59 @@
 from constraint.graph_generate import *
 from constraint.division_coding import *
 from constraint.fixed_length_coding import *
+import time
 
-vertex_set, edge_set, matrix_order, capacity = graph_generate(3, 3, 4, 1)
+# vertex_set, edge_set, matrix_order, capacity = graph_generate(3, 3, 4, 1)
+# division_coding(vertex_set, edge_set, capacity, 1)
+# fixed_length_coding(vertex_set, edge_set, capacity, 1)
 
+
+def checkPara(m, r, l, e, x):
+    if m & 1 == 0 or r > l or 2 * e > l + 1 or x < 0:
+        return "非法输入"
+    return "pass"
 
 def graph_generate_QT(m, r, l, e, x):
     vertex_set, edge_set, matrix_order, capacity = graph_generate(m, r, l, e, x)
     return len(vertex_set),len(vertex_set[0]),capacity;
 
-def division_coding_QT(m, r, l, e, x):
-    vertex_set, edge_set, matrix_order, capacity = graph_generate(m, r, l, e, x)
-    division_coding(vertex_set, edge_set, capacity, 3)
+# seq为输入的01序列，runCnt为准备运行的次数
 
-def fixed_length_coding_QT(m, r, l, e, x):
+# 最终的return值都以string的统一形式返回
+def division_coding_one_QT(m, r, l, e, x,seq):
     vertex_set, edge_set, matrix_order, capacity = graph_generate(m, r, l, e, x)
-    fixed_length_coding(vertex_set, edge_set, capacity, 3)
+    a,b,c = division_coding_one(vertex_set, edge_set,int(seq,2))
+    return a,b
+
+def fixed_length_coding_one_QT(m, r, l, e, x,seq):
+    vertex_set, edge_set, matrix_order, capacity = graph_generate(m, r, l, e, x)
+    return fixed_length_coding_one(vertex_set, edge_set,capacity,int(seq,2))
+def division_coding_QT(m, r, l, e, x,runCnt):
+    beginTime = time.time()
+    vertex_set, edge_set, matrix_order, capacity = graph_generate(m, r, l, e, x)
+    cap =  division_coding(vertex_set, edge_set, capacity, runCnt)
+    endTime = time.time()
+    time_str = "{:.4f}s".format(endTime - beginTime)
+    print(type(cap),endTime - beginTime)
+    return  cap,time_str
+
+def fixed_length_coding_QT(m, r, l, e, x,runCnt):
+    beginTime = time.time()
+    vertex_set, edge_set, matrix_order, capacity = graph_generate(m, r, l, e, x)
+    cap =  fixed_length_coding(vertex_set, edge_set, capacity, runCnt)
+    endTime = time.time()
+    time_str = "{:.4f}s".format(endTime - beginTime)
+    print(type(cap),endTime - beginTime)
+    return  cap,time_str
+
+
+
+# print(division_coding_one_QT(3, 3, 4, 1,1,"111111"))
+# print(fixed_length_coding_one_QT(3, 3, 4, 1,1,"0101010101"))
+
+
+
+#
+# #
+print(" res: " ,division_coding_QT(3, 3, 4, 1,0,20))
+print(" res: " ,fixed_length_coding_QT(3, 3, 4, 1,0,20))
--- source/constraint/fixed_length_coding.py
+++ source/constraint/fixed_length_coding.py
@@ -2,6 +2,74 @@ import math
 import random
 
 
+def fixed_length_coding_one(vertex_set, edge_set, capacity, num, l=8):
+    path_set = []
+    vertex_len = len(vertex_set[0])
+    for i in range(len(vertex_set)):
+        path = []
+        for vertex in edge_set[i]:
+            path.append(vertex_set[i] + vertex[vertex_len - 1])
+        path_set.append(path)
+
+    p = 2
+    # capacity = 2.573885669550532                          #3-SSA,3-RLL,(4,1)-局部GC约束的稀疏矩阵对应的谱半径
+    q = 99999999  # 防止第一次判定循环没设置q
+    while p / q < 0.85 * math.log2(capacity):
+        p += 1
+        p_len = pow(2, p)
+        # 统计每个点的出度
+        min_path_num = 99999999999
+        for i in path_set:
+            if min_path_num > len(i):
+                min_path_num = len(i)
+
+        # 对矩阵进行迭代，并保存路径
+        while min_path_num < p_len:
+            new_path_set = []
+            for i in range(len(vertex_set)):
+                new_path = []
+                for path in path_set[i]:
+                    last_vertex = path[len(path) - vertex_len:len(path)]
+                    last_vertex_index = vertex_set.index(last_vertex)
+                    for edge in edge_set[last_vertex_index]:
+                        new_path.append(path + edge[len(edge) - 1])
+                new_path_set.append(new_path)
+            path_set = new_path_set
+            min_path_num = 99999999999
+            for i in path_set:
+                if min_path_num > len(i):
+                    min_path_num = len(i)
+        q = len(path_set[0][0]) - len(vertex_set[0])
+        # print(len(path_set[0][0]) - 4)
+        # print(min_path_num)
+    print('选取的定长编码比例为：', p, ':', q, sep='')
+
+
+    vertex_index = 0  # 设定第一次进行编码的位置
+    print('编码的整数为：', num)
+    coding = ''
+    # for i in range(l):
+    while num!=0:
+        remainder = num % pow(2, p)
+        coding += path_set[vertex_index][remainder][len(vertex_set[0]):q + len(vertex_set[0])]
+        vertex_index = vertex_set.index(path_set[vertex_index][remainder][q:q + len(vertex_set[0])])
+        num = num >> p
+    print('编码结果为：', coding)
+    codingRes=coding
+    # 进行解码
+    num = 0
+    coding = vertex_set[0] + coding
+    # for i in range(l):
+    while coding != vertex_set[0]:
+        lenc = len(coding)
+        path = coding[lenc - len(path_set[0][0]):lenc]
+        pre_vertex = path[0:len(vertex_set[0])]
+        remainder = path_set[vertex_set.index(pre_vertex)].index(path)
+        num = remainder + (num << p)
+        coding = coding[0:len(coding) - q]
+    print('解码结果为：', num)
+    return codingRes,bin(num)[2:]
+
 def fixed_length_coding(vertex_set, edge_set, capacity, run_num, l=8):
     path_set = []
     vertex_len = len(vertex_set[0])
@@ -50,7 +118,8 @@ def fixed_length_coding(vertex_set, edge_set, capacity, run_num, l=8):
         num = random.randint(0, pow(2, p * l))
         print('随机选择的大整数为：', num)
         coding = ''
-        for i in range(l):
+        # for i in range(l):
+        while num!=0:
             remainder = num % pow(2, p)
             coding += path_set[vertex_index][remainder][len(vertex_set[0]):q + len(vertex_set[0])]
             vertex_index = vertex_set.index(path_set[vertex_index][remainder][q:q + len(vertex_set[0])])
@@ -60,7 +129,8 @@ def fixed_length_coding(vertex_set, edge_set, capacity, run_num, l=8):
         # 进行解码
         num = 0
         coding = vertex_set[0] + coding
-        for i in range(l):
+        # for i in range(l):
+        while coding != vertex_set[0]:
             lenc = len(coding)
             path = coding[lenc - len(path_set[0][0]):lenc]
             pre_vertex = path[0:len(vertex_set[0])]
@@ -68,4 +138,4 @@ def fixed_length_coding(vertex_set, edge_set, capacity, run_num, l=8):
             num = remainder + (num << p)
             coding = coding[0:len(coding) - q]
         print('解码结果为：', num)
-    return
+    return  p / q
